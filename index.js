37c18412f2f2c70874d1cf6e191d85fac6033754
Jéssica Pinheiro <109962238+JessicaPinheiro97@users.noreply.github.com>
2022-12-17 03:28:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var n1 = 10;
+var n2 = 20;
+console.log("Linha 1");
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+console.log("Linha 5");
+n1 = 100;
+n2 = 200;
+console.log('n1: ', n1);
+console.log('n2: ', n2);
+
+//Aprendizado sobre breakpoints
